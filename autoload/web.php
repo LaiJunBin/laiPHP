21301924f7f037a40e8364e93100529305b7b1ee
laijunbin <xyz607xx@gmail.com>
2019-05-12 22:39:53 +0800

    修改範例

--- autoload/web.php
+++ autoload/web.php
@@ -6,6 +6,7 @@
     // Route File
     // Route::method('url/{params}','controller@function');
     // Example:
-    Route::get('/','MainController@index');
-
     Route::get('/test', 'MainController@test');
+
+    Route::get('/','MainController@index');
+    Route::get('/{id}', 'MainController@get');
