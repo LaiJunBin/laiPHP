d847d0f45dc09c125d31831d60969c9c0031edab
laijunbin <xyz607xx@gmail.com>
2019-05-17 00:45:40 +0800

    完成 @foreach

--- autoload/lai.php
+++ autoload/lai.php
@@ -34,7 +34,29 @@
             }
 
             for($i = 0; $i < count($html_array); $i++){
-                if(mb_strpos(trim($html_array[$i]), '@for') === 0){
+                if(mb_strpos(trim($html_array[$i]), '@foreach') === 0){
+                    $stack = [];
+                    for($j = $i; $j < count($html_array); $j++){
+                        preg_match_all('/{/', $html_array[$j], $left);
+                        if(count($left[0]) > 0)
+                            array_push($stack, ...$left[0]);
+
+                        preg_match_all('/}/', $html_array[$j], $right);
+                        array_splice($stack, 0, count($right[0]));
+
+                        if(count($stack) == 0){
+                            $condition = self::get_condition($html_array[$i]);
+                            list($start, $end) = [$i, $j];
+                            $temp = array_slice($html_array, $i+1, $j-$i-1);
+                            $array = [];
+                            $index_variable = trim(mb_substr($condition, mb_strpos($condition, 'as')+2));
+                            $syntax = ('foreach('.$condition.'){ $params[mb_substr($index_variable,1)] = '.$index_variable.'; array_push($array, ...self::for_assign_variable($temp, $params, $index_variable)); }');
+                            eval($syntax);
+                            array_splice($html_array, $start, $end-$start+1, $array);
+                            break;
+                        }
+                    }
+                }else if(mb_strpos(trim($html_array[$i]), '@for') === 0){
                     $stack = [];
                     for($j = $i; $j < count($html_array); $j++){
                         preg_match_all('/{/', $html_array[$j], $left);
@@ -232,7 +254,7 @@
 
                 if(preg_match('/\\'.$index_variable.'\W/', $temp[$params['for1']])){
                     if(is_string($params[mb_substr($index_variable,1)])){
-                        $array[$params['for1']] = preg_replace('/\\'.$index_variable.'/', $params[mb_substr($index_variable,1)], $temp[$params['for1']]);
+                        $array[$params['for1']] = preg_replace('/\\'.$index_variable.'/', '\''.$params[mb_substr($index_variable,1)].'\'', $temp[$params['for1']]);
                     }else{
                         $array[$params['for1']] = preg_replace('/\\'.$index_variable.'/', '\''.$params[mb_substr($index_variable,1)].'\'', $temp[$params['for1']]);
                     }
