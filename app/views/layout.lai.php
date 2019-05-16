fd0c871c25879349b5253e573d84b73482666079
laijunbin <xyz607xx@gmail.com>
2019-05-17 01:43:42 +0800

    demo 基本模板語法

---
+++ app/views/layout.lai.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield(title)</title>
+    <link rel="stylesheet" href="./public/assets/css/main.css">
+</head>
+<body>
+    @yield(main)
+    <div>
+        <a href="https://github.com/LaiJunBin/laiPHP" class="btn">View Source</a>
+    </div>
+</body>
+</html>
--- app/views/welcome.lai.php
+++ app/views/welcome.lai.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="./public/assets/css/main.css">
-</head>
-<body>
-    <div>
-        Welcome {{ $id ?? null }}
-    </div>
-    <div>
-        <a href="https://github.com/xyz607xx/framework" class="btn">View Source</a>
-    </div>
-</body>
-</html>
+@extends(layout)
+
+@section(title, Welcome)
+
+@section(main){
+    <div>Welcome {{ $id ?? null }}</div>
+}
