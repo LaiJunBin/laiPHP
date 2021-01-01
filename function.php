4ea889c3591ef594430f18216040350ea33502ed
laijunbin <xyz607xx@gmail.com>
2021-01-01 18:55:27 +0800

    加強dd功能

--- function.php
+++ function.php
@@ -1,12 +1,121 @@
 <?php
-
     // 經常使用的方法定義在這邊
 
+    $dd_options = [
+        'max_object_depth' => 10,
+        'tab_size' => 4,
+        'pre_style' => 'background:#333; font-size:16px; color:#fff; padding:10px;',
+        'styles' => [
+            // 'error' => 'color: #ff0000;',
+            // 'index' => 'color: #a71d5d;',
+            // 'integer' => 'color: #56aaff',
+            // 'string' => 'color: #f3a999',
+            // 'boolean' => 'color: #ffe100',
+            // 'default' => 'color: #2cef57;',
+            'index' => 'color: #f3a999',
+            'error' => 'color: #ff0000;',
+            'object' => 'color: #56aaff;',
+            'default' => 'color: #2cef57;',
+            'fold' => 'color: #ffe100;',
+        ]
+    ];
+
     function dd(...$params){
+        global $dd_options;
         foreach($params as $param){
-            var_dump($param);
+            echo '<pre style="'.$dd_options['pre_style'].'">';
+            ddp($param);
+            echo '</pre>';
         }
         exit;
+    }
+
+    function dump(...$params){
+        global $dd_options;
+        foreach($params as $param){
+            echo '<pre style="'.$dd_options['pre_style'].'">';
+            ddp($param);
+            echo '</pre>';
+        }
+    }
+
+    function ddp($param, $tab=0, $object_depth=1, $close=false){
+        global $dd_options;
+
+        if($object_depth >= $dd_options['max_object_depth']){
+            return;
+        }
+        if(is_object($param)){
+            $count = is_iterable($param) ? iterator_count($param) : count(get_object_vars($param));
+            $custom_params = [];
+            if($param instanceof DB){
+                $object_depth++;
+                foreach(get_class_methods($param) as $method){
+                    if(in_array($method, get_class_methods('DB'))){
+                        continue;
+                    }
+                    try {
+                        $custom_params[$method] = call_user_func([$param, $method]);
+                    } catch (\Throwable $th) {
+                    }
+                }
+            }
+            $count += count($custom_params);
+
+            echo '<span style="'.$dd_options['styles']['object'].'">';
+            echo 'object('.get_class($param).')#'.spl_object_id($param).' ('.$count.') ';
+            echo "</span>";
+            if($close){
+                echo '<span style="'.$dd_options['styles']['fold'].'cursor:pointer;" onclick="this.innerHTML=this.innerHTML===\'(Open)<br>\'?\'(Close) ';
+                echo '<span style=color:#fff>{</span>';
+                echo '<br>\':\'(Open)<br>\';this.nextSibling.style.display=this.nextSibling.style.display===\'none\'?\'block\':\'none\';">(Open)<br></span><div style="display: none;">';
+            } else if($object_depth <= $dd_options['max_object_depth'] - 1){
+                echo str_repeat(' ', $tab).'{ <br>';
+            }
+            if($object_depth >= $dd_options['max_object_depth']){
+                echo '<span style="'.$dd_options['styles']['error'].'">'.str_repeat(' ', $tab+$dd_options['tab_size']) .' dump object depth exceeded. </span><br>';
+            }else{
+                foreach($param as $key => $value){
+                    echo str_repeat(' ', $tab+$dd_options['tab_size']) . '<span style="'.$dd_options['styles']['index'].'">["'.$key. '"]</span> => ';
+                    ddp($value, $tab+$dd_options['tab_size'], $object_depth, true);
+                }
+                foreach($custom_params as $key => $value){
+                    echo str_repeat(' ', $tab+$dd_options['tab_size']) . '<span style="'.$dd_options['styles']['index'].'">["'.$key. '"]</span> => ';
+                    ddp($value, $tab+$dd_options['tab_size'], $object_depth, true);
+                }
+                if(!$close)
+                    echo str_repeat(' ', $tab).'}<br>';
+            }
+            if($close){
+                echo str_repeat(' ', $tab).'}</div>';
+            }
+        } else if(is_array($param)){
+            echo '<span style="'.$dd_options['styles']['object'].'">';
+            echo "array(".count($param).") ";
+            echo '</span>';
+            if($close){
+                echo '<span style="'.$dd_options['styles']['fold'].'cursor:pointer;" onclick="this.innerHTML=this.innerHTML===\'(Open)<br>\'?\'(Close) ';
+                echo '<span style=color:#fff>{</span>';
+                echo '<br>\':\'(Open)<br>\';this.nextSibling.style.display=this.nextSibling.style.display===\'none\'?\'block\':\'none\';">(Open)<br></span><div style="display: none;">';
+            } else {
+                echo '{ <br>';
+            }
+            foreach($param as $key => $value){
+                    echo str_repeat(' ', $tab+$dd_options['tab_size']) . '<span style="'.$dd_options['styles']['index'].'">["'.$key. '"]</span> => ';
+                ddp($value, $tab+$dd_options['tab_size'], $object_depth, true);
+            }
+            if($close){
+                echo str_repeat(' ', $tab).'}</div>';
+            }else{
+                echo str_repeat(' ', $tab).'}<br>';
+            }
+        } else {
+            $type = gettype($param);
+            $style = $dd_options['styles'][$type] ?? $dd_options['styles']['default'];
+            echo '<span style="'.$style.'">';
+            var_dump($param);
+            echo '</span>';
+        }
     }
 
     function keys($array){
