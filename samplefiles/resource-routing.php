b1634de394ab85e1d2b68b7d8a12b922ad6c9776
laijunbin <xyz607xx@gmail.com>
2021-01-03 20:43:52 +0800

    update route order

--- samplefiles/resource-routing.php
+++ samplefiles/resource-routing.php
@@ -2,8 +2,8 @@
     // ------- CLI Generate Routing (start) -------
     Route::group("/%s", function(){
         Route::get("/", "%s@index");
-        Route::get("/{id}", "%s@show");
         Route::get("/create", "%s@create");
+        Route::get("/{id}", "%s@show");
         Route::post("/", "%s@store");
         Route::get("/{id}/edit", "%s@edit");
         Route::patch("/{id}", "%s@update");
