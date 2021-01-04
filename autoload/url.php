3d7e7e4b8896ea18f99f8869860e07c884131ae4
laijunbin <xyz607xx@gmail.com>
2021-01-04 21:51:03 +0800

    url class

---
+++ autoload/url.php
@@ -0,0 +1,41 @@
+<?php
+
+    class URL {
+        public $url;
+        public function __construct($path=''){
+            $is_cli_server = php_sapi_name() == 'cli-server';
+            $path = array_filter(explode('/', $path), function($x){
+                return $x !== '.';
+            });
+
+            clearEmpty($path);
+            $path = implode('/', $path);
+
+            if(!$is_cli_server){
+                $current_dir = str_replace('\\','/',getcwd());
+                $root = $_SERVER['DOCUMENT_ROOT'];
+                $except_url = explode('/',str_replace($root,'',$current_dir));
+
+                clearEmpty($except_url);
+                $path = explode('/', ('/'.implode('/', $except_url).'/'.$path));
+                clearEmpty($path);
+                $path = implode('/', $path);
+                $this->url = '/'.$path;
+            }else{
+                $this->url = $path;
+            }
+
+        }
+
+        public function current(){
+            return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        }
+
+        public function previous(){
+            return $_SERVER['HTTP_REFERER'];
+        }
+
+        public function __toString(){
+            return $this->url;
+        }
+    }
