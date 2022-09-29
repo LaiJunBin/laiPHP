25ee31fd1722cb0d5b79a40b2bef8998a8a67405
laijunbin <xyz607xx@gmail.com>
2022-09-29 20:28:51 +0800

    add some packages.

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,10 @@ import Bootstrap from 'bootstrap/dist/js/bootstrap'
 import Chart from 'chart.js/dist/chart'
 import 'bootstrap/dist/css/bootstrap.css'
 import WOW from 'wow.js/dist/wow'
+import * as THREE from 'three';
+import anime from 'animejs/lib/anime.es.js';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import * as Gsap from "gsap/all";
 
-export { Vue, Bootstrap, Chart, WOW }
+export { Vue, Bootstrap, Chart, WOW, THREE, anime, AOS, Gsap }
